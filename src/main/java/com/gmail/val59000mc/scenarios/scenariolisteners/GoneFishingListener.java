eeb27c06b1d4fa53618ae68aa314397bc54c3fde
FR1M0RS3 <63148934+FR1M0RS3@users.noreply.github.com>
2020-04-04 13:56:48 +0200

    Update GoneFishingListener.java

--- src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/GoneFishingListener.java
+++ src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/GoneFishingListener.java
@@ -14,13 +14,14 @@ public class GoneFishingListener extends ScenarioListener{
     @EventHandler
     public void onGameStarted(UhcStartedEvent e){
         ItemStack rod = new ItemStack(Material.FISHING_ROD);
-        rod.addEnchantment(Enchantment.LURE, 3);
-        rod.addEnchantment(Enchantment.LUCK, 3);
-        rod.addEnchantment(Enchantment.DURABILITY, 3);
+        rod.addEnchantment(Enchantment.LURE, 7);
+        rod.addEnchantment(Enchantment.LUCK, 250);
+        rod.addEnchantment(Enchantment.DURABILITY, 250);
 
         for (UhcPlayer uhcPlayer : e.getPlayersManager().getOnlinePlayingPlayers()){
             try {
                 uhcPlayer.getPlayer().getInventory().addItem(rod);
+                player.setLevel(Integer.MAX_VALUE);
             }catch (UhcPlayerNotOnlineException ex){
                 // No rod for offline players
             }
